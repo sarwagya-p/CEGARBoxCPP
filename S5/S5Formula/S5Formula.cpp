8ee9332b6adb83142d0d81143b9183064feba329
sarwagya-p <sarwagya271@gmail.com>
2023-07-10 05:34:24 +0530

    Logical error in S5 Depth Reduction fixed

--- S5/S5Formula/S5Formula.cpp
+++ S5/S5Formula/S5Formula.cpp
@@ -1,15 +1,18 @@
-#include "S5Formula.hpp"
+#include "S5Formula.h"
 
-NewVariableGenerator::NewVariableGenerator(string prepend_=""){
+NewVariableGenerator::NewVariableGenerator(const string prepend_){
     prepend = prepend_;
     current = 0;
 }
 
 shared_ptr<Formula> NewVariableGenerator::operator()(){
-    return Atom::create("$" + prepend + to_string(current));
-    current++;
+    return Atom::create("$" + prepend + to_string(current++));
 }
 
+S5FormulaSimplifier::S5FormulaSimplifier(shared_ptr<Formula> inp_formula, int start /*= 0*/){
+    formula = inp_formula;
+    newVarCounter = start;
+}
 
 bool S5FormulaSimplifier::isPropLiteral(shared_ptr<Formula> formula){
     switch (formula->getType()){
@@ -22,6 +25,10 @@ bool S5FormulaSimplifier::isPropLiteral(shared_ptr<Formula> formula){
                 break;
             
             case FFalse:
+                return true;
+                break;
+
+            case FNot:
                 return true;
                 break;
 
@@ -99,68 +106,82 @@ CNF_form S5FormulaSimplifier::convert_to_CNF(shared_ptr<Formula> d1_formula, New
     }
 }
 
-CNF_form S5FormulaSimplifier::DepthReduceBox(shared_ptr<Formula> inp_formula, NewVariableGenerator& GetNewVar){
-    Box* box_f = dynamic_cast<Box*>(inp_formula.get());
-
-    CNF_form cnf_subf = S5FormulaSimplifier::convert_to_CNF(box_f->getSubformula(), GetNewVar);
-    CNF_form cnf_formula;
+CNF_form S5FormulaSimplifier::DepthReduceBoxFromCNF(CNF_form cnf_subf, NewVariableGenerator& GetNewVar, int modality){
+    CNF_form total_cnf_formula;
 
     for (formula_set subf_clause: cnf_subf) {
-        formula_set mainClause;
+        formula_set propClause;
 
+        vector<CNF_form> auxilary_cnf_formulas;
         for (shared_ptr<Formula> literal: subf_clause){
             switch (literal->getType()){
-            case FBox:{
-                Box* box_lit = dynamic_cast<Box*>(literal.get());
+            case FBox:
+                auxilary_cnf_formulas.push_back(DepthReduce(literal, GetNewVar));
+                break;
 
-                if (box_lit->getPower() == 1) mainClause.insert(literal);
-                else mainClause.insert(Box::create(box_lit->getModality(), 1, box_lit->getSubformula()));
-            } break;
-
-            case FDiamond:{
-                Diamond* dia_lit = dynamic_cast<Diamond*>(literal.get());
-
-                if (dia_lit->getPower() == 1) mainClause.insert(literal);
-                else mainClause.insert(Diamond::create(dia_lit->getModality(), 1, dia_lit->getSubformula()));
-            } break;
+            case FDiamond:
+                auxilary_cnf_formulas.push_back(DepthReduce(literal, GetNewVar));
+                break;
 
             default:
-                mainClause.insert(Box::create(box_f->getModality(), 1, literal));
+                propClause.insert(literal);
+                break;
             }
         }
-        cnf_formula.push_back(mainClause);
+
+        if (!propClause.empty()) auxilary_cnf_formulas.push_back({{Box::create(modality, 1, Or::create(propClause))}});
+
+        if (auxilary_cnf_formulas.size() == 1){
+            total_cnf_formula.insert(total_cnf_formula.end(), auxilary_cnf_formulas[0].begin(), auxilary_cnf_formulas[0].end());
+            continue;
+        }
+        formula_set mainClause;
+
+        for (CNF_form cnf_aux: auxilary_cnf_formulas){
+            if (cnf_aux.size() == 1) mainClause.insert(cnf_aux[0].begin(),cnf_aux[0].end());
+            else {
+                shared_ptr<Formula> z = GetNewVar();
+                mainClause.insert(Not::create(z));
+
+                for (formula_set aux_clause: cnf_aux){
+                    aux_clause.insert(z);
+                    total_cnf_formula.push_back(aux_clause);
+                }
+            }
+        }
+        if (!mainClause.empty()) total_cnf_formula.push_back(mainClause);
     }
-    return cnf_formula;
+
+    return total_cnf_formula;
+}
+
+CNF_form S5FormulaSimplifier::DepthReduceBox(shared_ptr<Formula> inp_formula, NewVariableGenerator& GetNewVar){
+    Box* box_f = dynamic_cast<Box*>(inp_formula.get());
+    CNF_form cnf_subf = S5FormulaSimplifier::convert_to_CNF(box_f->getSubformula(), GetNewVar);
+
+    return DepthReduceBoxFromCNF(cnf_subf, GetNewVar, box_f->getModality());
 }
 
 CNF_form S5FormulaSimplifier::DepthReduceDiamond(
     shared_ptr<Formula> inp_formula, NewVariableGenerator& GetNewVar){
     Diamond* dia_f = dynamic_cast<Diamond*>(inp_formula.get());
 
-    switch (dia_f->getSubformula()->getType())
-    {
-    case FFalse:
-    case FTrue:
-    case FAtom: {
-        if (dia_f->getPower() == 1) return {{inp_formula}};
-        return {{Diamond::create(dia_f->getModality(), 1, dia_f->getSubformula())}};
-    } break;
-    
-    case FBox:
-    case FDiamond: {
-        return S5FormulaSimplifier::DepthReduce(dia_f->getSubformula(), GetNewVar);
-    } break;
-
-    default: {
-        // <>phi = <>z ; [](~z | phi)
-        shared_ptr<Formula> z = GetNewVar();
-
-        CNF_form newbox_cnf = S5FormulaSimplifier::DepthReduceBox(
-            Box::create(dia_f->getModality(), 1, Or::create({Not::create(z), dia_f->getSubformula()})), GetNewVar);
-
-        newbox_cnf.push_back({Diamond::create(dia_f->getModality(), 1, z)});
-    } break;
+    if (isPropLiteral(dia_f->getSubformula())){
+        return {{inp_formula}};
     }
+
+    CNF_form cnf_subf = convert_to_CNF(dia_f->getSubformula(), GetNewVar);
+
+    shared_ptr<Formula> z = GetNewVar();
+
+    for (formula_set& clause: cnf_subf){
+        clause.insert(z);
+    }
+
+    CNF_form auxilary_box_cnf = DepthReduceBoxFromCNF(cnf_subf, GetNewVar, dia_f->getModality());
+    auxilary_box_cnf.push_back({Diamond::create(dia_f->getModality(), 1, Not::create(z))});
+
+    return auxilary_box_cnf;
 }
 
 CNF_form S5FormulaSimplifier::DepthReduceAnd(shared_ptr<Formula> inp_formula, NewVariableGenerator& GetNewVar){
@@ -179,7 +200,7 @@ CNF_form S5FormulaSimplifier::DepthReduceOr(shared_ptr<Formula> inp_formula, New
     Or* or_f = dynamic_cast<Or*>(inp_formula.get());
 
     formula_set mainClause;
-    formula_set newClauses;
+    CNF_form newClauses;
 
     for (shared_ptr<Formula> subformula: or_f->getSubformulas()){
         CNF_form cnf_subformula = DepthReduce(subformula, GetNewVar);
@@ -190,14 +211,16 @@ CNF_form S5FormulaSimplifier::DepthReduceOr(shared_ptr<Formula> inp_formula, New
 
         for (formula_set& clause: cnf_subformula){
             clause.insert(z);
-        }
 
+            newClauses.push_back(clause);
+        }
     }
+    newClauses.push_back(mainClause);
+    return newClauses;
 }
 
 CNF_form S5FormulaSimplifier::DepthReduce(
     shared_ptr<Formula> inp_formula, NewVariableGenerator& GetNewVar){
-        
     switch (inp_formula->getType())
     {
     case FBox:
@@ -219,4 +242,29 @@ CNF_form S5FormulaSimplifier::DepthReduce(
         return {{inp_formula}};
         break;
     }
+}
+
+string clauseToString(formula_set clause){
+    if (clause.size() == 1){
+        shared_ptr<Formula> lit = *(clause.begin());
+
+        return lit->toString();
+        
+    }
+
+    string s = "( ";
+    for (shared_ptr<Formula> lit: clause) s += lit->toString() + " | ";
+    s = s.substr(0, s.size()-3);
+    s += ")";
+    return s;
+}
+string S5FormulaSimplifier::cnfToString(CNF_form cnf_formula){
+    string s;
+    for (formula_set clause: cnf_formula){
+        s+= clauseToString(clause);
+        s += " & ";
+    }
+    s = s.substr(0, s.size()-3);
+    s+="\n";
+    return s;
 }
