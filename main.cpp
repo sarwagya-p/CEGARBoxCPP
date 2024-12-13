90384355055726835244038769c93ccc8b7e76ad
Sarwagya Prasad <sarwagyaprasad@Sarwagyas-MacBook-Pro.local>
2024-12-14 00:53:23 +0530

    test commit

--- main.cpp
+++ main.cpp
@@ -109,6 +109,7 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
     return 0;
 }
 
+
 static struct argp argp = {options, parse_opt, args_doc, doc, 0, 0, 0};
 
 void solve(arguments_struct &args);
